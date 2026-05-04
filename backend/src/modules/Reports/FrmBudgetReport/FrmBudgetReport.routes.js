72bc08457420cd770386a6c9162017fc842b004a
Yogesh-Ascentech <yogesh.bhagat@ascentechindia.com>
2026-05-04 12:58:03 +0530

    auth added

--- backend/src/modules/Reports/FrmBudgetReport/FrmBudgetReport.routes.js
+++ backend/src/modules/Reports/FrmBudgetReport/FrmBudgetReport.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./FrmBudgetReport.controller");
+const auth = require("../../../middlewares/auth.middleware");
 
 router.post("/budget-report-pdf", auth(), controller.getBudgetReportPDF);
 
--- backend/src/modules/Reports/RptGovtTaxRegisters/RptGovtTaxRegister.route.js
+++ backend/src/modules/Reports/RptGovtTaxRegisters/RptGovtTaxRegister.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./RptGovtTaxRegister.controller");
+const auth = require("../../../middlewares/auth.middleware");
 
 router.post("/govt-tax-register-pdf1", auth(), controller.getGovtTaxRegisterPDF1);
 router.post("/govt-tax-register-summary-pdf", auth(), controller.getGovtTaxRegisterSummaryPDF);
